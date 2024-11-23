17d7e99a49f905fb6d85677bf055f1b23340a8ef
Korovina Liza <144222955+korovinaen@users.noreply.github.com>
2024-11-24 01:20:27 +0300

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,8 +1,3 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-volume = 1.44 * (2 ** 20)
-number_pages = 100
-strings_on_page = 50
-symbols_in_string = 25
-count_symbols = (number_pages * strings_on_page * symbols_in_string) * 4
-count_books = int(volume // count_symbols)
-print("Количество книг, помещающихся на дискету:", count_books)
+a = 1.44*1024*1024/25/50/100/4
+print("Количество книг, помещающихся на дискету:", int(a))
